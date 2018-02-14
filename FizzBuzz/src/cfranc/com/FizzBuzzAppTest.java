248011c83bd907a57028e9523e81538cd49b6bf0
dkrychylskyy <d.krychylskyy@gmail.com>
2018-02-14 14:07:38 +0100

    Ajouter 'public' á la classe FizzBuzzAppTest

--- FizzBuzz/src/cfranc/com/FizzBuzzAppTest.java
+++ FizzBuzz/src/cfranc/com/FizzBuzzAppTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class FizzBuzzAppTest {
+public class FizzBuzzAppTest {
 
 	FizzBuzzApp app = new FizzBuzzApp();
 	
